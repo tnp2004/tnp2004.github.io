3ee9aecc027d5b7dee24117856ab5435e6d8895b
tnp2004 <iceandiceisice2004@gmail.com>
2023-10-05 21:34:57 +0700

    :sparkles:github page deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tnp2004.github.io',
+});
