04175b147d8f43fd68a027f3cfc687cca0254990
tnp2004 <iceandiceisice2004@gmail.com>
2023-10-06 16:02:03 +0700

    :sparkles:greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tnp2004.github.io',
+  site: 'https://tnp2004.github.io',
+  integrations: [tailwind()]
 });
